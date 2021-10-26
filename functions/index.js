811d75142b4c617880ee10a42eb1d06b71c96403
ChristinaKamel <christina.kamel@isae.edu.lb>
2021-10-26 05:52:57 +0300

    projet

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,4 @@
 const getFacture = require('./getFacture');
+const declencherNotification = require('./declencherNotification')
 exports.getFacture = getFacture.getFacture;
+exports.declencherNotification = declencherNotification.declencherNotification;
